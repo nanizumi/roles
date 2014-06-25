b7f0496060eeb31e37767e0e56129449d560e8a3
nanizumi <nanizumi@gmail.com>
2014-06-25 17:37:14 +0100

    added roles

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,1 +1,34 @@
 //insert fixtures here
+if(AppControl.find().count() === 0 )
+{
+  //add data to appcontrol
+
+  Accounts.createUser({
+    'username' : 'admin',
+    'email' : 'admin@doe.com',
+    'password' : '123123', //encrypted automatically
+    'profile': {
+      'name': 'Administrator'
+    }
+  });
+
+  Accounts.createUser({
+    'username' : 'jon.doe',
+    'email' : 'john@doe.com',
+    'password' : 'abc123', //encrypted automatically
+    'profile': {
+      'name': 'Jonh Doe'
+    }
+  });
+
+  Accounts.createUser({
+    'username' : 'test',
+    'email' : 'test@home.com',
+    'password' : '123123', //encrypted automatically
+    'profile': {
+      'name': 'Test User'
+    }
+  });
+
+  AppControl.insert({what: 'something'});
+}
