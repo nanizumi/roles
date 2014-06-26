24b025f2753c844f501592f61755b30bc67a7d45
nanizumi <nanizumi@gmail.com>
2014-06-26 13:08:41 +0100

    rearranged fixtures tradução macaca!

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,2 +1,10 @@
 Template.header.helpers({
 });
+
+
+//solução do macaco!!! não funciona nada bem...
+Template.header.rendered = function() {
+  $('#login-buttons-password').text('entrar');
+  $('#login-username').attr('placeholder', 'nome de utilizador');
+  $('#login-password').attr('placeholder', 'palavra chave');
+};
---
+++ run-debug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#http://stackoverflow.com/questions/14751080/how-can-i-debug-my-meteor-app-using-the-webstorm-ide
+NODE_OPTIONS=--debug=47977 meteor  # Linux/Mac
